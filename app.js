4feba1b04dcb6b4fb0343a9bd396acca1b308589
EinarLop <einarlop0@gmail.com>
2022-10-29 18:57:36 -0500

    localhost fix

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 const axios = require("axios");
-require('dotenv').config;
+require("dotenv").config;
+
 const token = process.env.TOKEN;
 const mytoken = process.env.MYTOKEN;
 
@@ -14,8 +15,8 @@ const messagesController = require("./controllers/messages.controller");
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -25,61 +26,66 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/messages", messagesController);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(3000 || process.env.PORT,()=>{
-    console.log("jala")
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get("/webhook",(req,res)=>{
-    let mode = req.query["hub.mode"];
-    let challenge = req.query["hub.challenge"];
-    let token = req.query["hub.verify_token"];
+// app.listen(3000 || process.env.PORT, () => {
+//   console.log("jala");
+// });
 
-    if (mode && token){
-        if (mode === "subscribe" && token ===mytoken){
-            res.status(200).send(challenge);
-            
-        }else{
-            res.status(403);
-        }
+app.get("/webhook", (req, res) => {
+  let mode = req.query["hub.mode"];
+  let challenge = req.query["hub.challenge"];
+  let token = req.query["hub.verify_token"];
+
+  if (mode && token) {
+    if (mode === "subscribe" && token === mytoken) {
+      res.status(200).send(challenge);
+    } else {
+      res.status(403);
     }
+  }
 });
 
-app.post("/webhook", (req,res)=>{
-    let body_param = req.body;
+app.post("/webhook", (req, res) => {
+  let body_param = req.body;
 
-    console.log(JSON.stringify(body_param,null,2));
+  console.log(JSON.stringify(body_param, null, 2));
 
-    if (body_param.entry &&
-        body_param.entry[0].changes &&
-        body_param.entry[0].changes[0].value.message &&
-        body_param.entry[0].changes[0].value.message[0]
-        ){
-            let phone_no_id = req.body.entry[0].changes[0].value.metadata.phone_number_id;
-            let from = req.body.entry[0].changes[0].value.messages[0].from;
-            let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body;
-            
-            axios({
-                method:"POST",
-                url:"https://graph.facebook.com/v13.0/"+phone_no_id+"/messages?access_token="+token,
-                data:{
-                    messagin_product:"whatsapp",
-                    to: from,
-                    text:{
-                        body: "Hi, im Alex"
-                    }
-                },
-                headers : {
-                    "Content-Type": "application/json"
-                } 
-            })
-        res.sendStatus(200);
-    }else{
-        res.sendStatus(403);
-    }
+  if (
+    body_param.entry &&
+    body_param.entry[0].changes &&
+    body_param.entry[0].changes[0].value.message &&
+    body_param.entry[0].changes[0].value.message[0]
+  ) {
+    let phone_no_id =
+      req.body.entry[0].changes[0].value.metadata.phone_number_id;
+    let from = req.body.entry[0].changes[0].value.messages[0].from;
+    let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body;
+
+    axios({
+      method: "POST",
+      url:
+        "https://graph.facebook.com/v13.0/" +
+        phone_no_id +
+        "/messages?access_token=" +
+        token,
+      data: {
+        messagin_product: "whatsapp",
+        to: from,
+        text: {
+          body: "Hi, im Alex",
+        },
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(403);
+  }
 });
 
 // catch 404 and forward to error handler
