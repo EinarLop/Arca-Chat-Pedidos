a8bbae5dda9ad6cc74873d9287b47aa986708a31
EinarLop <einarlop0@gmail.com>
2022-11-01 18:12:05 -0600

    checking env

--- app.js
+++ app.js
@@ -11,22 +11,21 @@ const token = process.env.TOKEN;
 const mytoken = process.env.MYTOKEN;
 
 const messagesController = require("./controllers/messages.controller");
-
 var app = express();
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/messages", messagesController);
-
+let pop = process.env.EINAR || "NO ENV";
 app.get("/", (req, res) => {
-  res.render("index", { title: "Express" });
+  res.send("Team One Conversation AI" + pop);
 });
 
 app.get("/webhook", (req, res) => {
@@ -84,9 +83,9 @@ app.post("/webhook", (req, res) => {
 });
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function (err, req, res, next) {
